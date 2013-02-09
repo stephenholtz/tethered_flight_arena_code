45f62b342aa5996723e63083096c813569c02ed2
Stephen Holtz <stephenlholtz@gmail.com>
2013-02-09 12:53:46 -0500

    ON OFF v01 is probably working properly. testing commit now2

--- position_functions/ON_OFF_set_v01/make_on_off_set_pos_funcs_v01.m
+++ position_functions/ON_OFF_set_v01/make_on_off_set_pos_funcs_v01.m
@@ -51,59 +51,66 @@ for time_between = time_between_set
 
 end
 
-% make the edge convergence/divergence position function
-
-degrees_per_step = 3.75;
-dps_set = [80 220];
-
-for dps_wanted = dps_set
-
-    ms_per_step = ((dps_wanted/degrees_per_step)/1000)^-1;
-    samples_per_step = ms_per_step*(1/ms_per_sample);
-
-    actual_samples_per_step = round(samples_per_step);
-    actual_dps = 1000*(ms_per_sample * actual_samples_per_step * (1/degrees_per_step))^-1;
-
-    real_num_steps = round(ms_per_step/ms_per_sample);
-    real_ms_per_step = real_num_steps*ms_per_step;
-    real_dps = degrees_per_step * real_ms_per_step;
-
-    time_before = 100;
-    
-    before = ones(1,time_before/ms_per_sample);
-    during = [];
-
-    for pos = 2:97
-
-        during = [during pos*ones(1,real_num_steps)];
-
-    end
-
-    after = ones(1,time_after/ms_per_sample);
-
-    stim_time = 1000*(numel(during) + numel(before + numel(after)));
-
-    func = [before during after] - 1;
-
-    if numel(num2str(func_iter)) < 2
-        count = ['00' num2str(func_iter)];
-    elseif numel(num2str(func_iter)) < 3
-        count = ['0' num2str(func_iter)];        
-    else
-        count = num2str(func_iter);
-    end
-
-    func_name = ['position_function_' count '_ON_OFF_edges_w_96_pos' num2str(time_before) 'ms_before_' num2str(round(actual_dps)) 'real_dps_' freq '_total_dur' num2str(stim_time) 'ms']; %#ok<*AGROW>
-
-    disp(func_name)
-
-    save(fullfile(output_location,func_name),'func')
-
-    func_iter = func_iter + 1;
-
-    clear before during after func_name func
-
-end
+% % make the edge convergence/divergence position function
+% 
+% degrees_per_step = 3.75;
+% time_after = 20;
+% dps_set = [80 220];
+% 
+% for dps_wanted = dps_set
+% 
+%     wanted_steps_per_ms = (dps_wanted * (1/degrees_per_step))/1000;
+%     samples_per_step = round(ms_per_sample/wanted_steps_per_ms);
+%     
+% 
+% %     
+% %     ms_per_step = ((dps_wanted/degrees_per_step)/1000)^-1;
+% %     samples_per_step = ms_per_step*(1/ms_per_sample);
+% % 
+% %     actual_samples_per_step = round(samples_per_step);
+%      actual_dps = 1000*(ms_per_sample * samples_per_step * (1/degrees_per_step))^-1;
+% % 
+% %     real_num_steps = round(ms_per_step/ms_per_sample);
+% %     real_ms_per_step = real_num_steps*ms_per_step;
+% %     real_dps = degrees_per_step * real_ms_per_step;
+% 
+%     time_before = 100;
+%     
+%     before = ones(1,time_before/ms_per_sample);
+%     during = [];
+% 
+%     for pos = 2:97
+% 
+%         during = [during pos*ones(1,samples_per_step)];
+% 
+%     end
+% 
+%     after = ones(1,round(time_after/ms_per_sample));
+%     actual_time_after = round(time_after/ms_per_sample)*ms_per_sample;
+%     
+%     stim_time = (numel(during) + numel(before + numel(after)))*ms_per_sample;
+% 
+%     func = [before during after] - 1;
+% 
+%     if numel(num2str(func_iter)) < 2
+%         count = ['00' num2str(func_iter)];
+%     elseif numel(num2str(func_iter)) < 3
+%         count = ['0' num2str(func_iter)];        
+%     else
+%         count = num2str(func_iter);
+%     end
+% 
+%     func_name = ['position_function_' count '_ON_OFF_edges_w_96_pos' num2str(time_before) 'ms_before_' num2str(round(actual_dps)) 'real_dps_' freq '_total_dur' num2str(stim_time) 'ms']; %#ok<*AGROW>
+% 
+%     disp(func_name)
+% 
+%     save(fullfile(output_location,func_name),'func')
+% 
+%     func_iter = func_iter + 1;
+% 
+%     clear before during after func_name func
+% 
+% end
 
 
 
--- protocols/ON_OFF_set_v01/ON_OFF_set_v01.m
+++ protocols/ON_OFF_set_v01/ON_OFF_set_v01.m
@@ -27,12 +27,13 @@ panel_cfgs = what(panel_cfgs_loc);
 panel_cfgs = panel_cfgs.mat;
 cd(cf);
 
-% Minimal Motion stimuli
-duration = .34;
+% Minimal Motion stimuli - WORKING
+duration = .4;
 default_frequency = 250;
 cond_num = 1;
 total_ol_dur = 0;
-
+if 1
+    
 for time_between_motion = 1:3 % 44 76 and 116 ms
     for arena_position = [3 5 7 9 11]
         for pattern = [3 4 5 6]
@@ -59,26 +60,26 @@ for time_between_motion = 1:3 % 44 76 and 116 ms
     end
 end
 
-% ON and OFF Edges
-for dps = 4:5 % 78 and 234 dps of the bar
+% ON and OFF Edges 
+for dps = round([100 220]/3.75) % 78 and 234 dps of the bar
     for pattern = 9:10 % converging vs diverging
         for on_off = 1:2 % y position has symm versions
             
             Conditions(cond_num).PatternID      = pattern; %#ok<*AGROW>
             Conditions(cond_num).PatternName    = patterns{pattern};
-            Conditions(cond_num).Gains          = [0 0 0 0];
-            Conditions(cond_num).Mode           = [4 0];
+            Conditions(cond_num).Gains          = [dps 0 0 0];
+            Conditions(cond_num).Mode           = [0 0];
             Conditions(cond_num).InitialPosition= [1 on_off];
             Conditions(cond_num).PosFuncLoc     = pos_func_loc;
-            Conditions(cond_num).PosFunctionX   = [1 dps];
+            Conditions(cond_num).PosFunctionX   = [1 0];
             Conditions(cond_num).FuncFreqX 		= default_frequency;
-            Conditions(cond_num).PosFuncNameX   = position_functions{dps};
+            Conditions(cond_num).PosFuncNameX   = 'null';%position_functions{dps};
 
             Conditions(cond_num).PosFunctionY 	= [2 0];
             Conditions(cond_num).FuncFreqY 		= default_frequency;
             Conditions(cond_num).PosFuncNameY   = 'null';
 
-            Conditions(cond_num).Duration       = duration;
+            Conditions(cond_num).Duration       = 100/dps;
             Conditions(cond_num).note           = '';
 
             total_ol_dur = total_ol_dur + Conditions(cond_num).Duration + .02;
@@ -88,24 +89,24 @@ for dps = 4:5 % 78 and 234 dps of the bar
 end
 
 % Sweeping bar
-for dps = 4:5 % 78 and 234 dps of the bar
+for dps = round([100 220]/3.75)%4:5 % 78 and 234 dps of the bar
     for on_off = 1:2 % y position has on vs off, 4px wide
         for pattern = 11:12 % CW CCW
                     Conditions(cond_num).PatternID      = pattern; %#ok<*AGROW>
                     Conditions(cond_num).PatternName    = patterns{pattern};
-                    Conditions(cond_num).Gains          = [0 0 0 0];
-                    Conditions(cond_num).Mode           = [4 0];
+                    Conditions(cond_num).Gains          = [dps 0 0 0];
+                    Conditions(cond_num).Mode           = [0 0];
                     Conditions(cond_num).InitialPosition= [1 on_off];
                     Conditions(cond_num).PosFuncLoc     = pos_func_loc;
-                    Conditions(cond_num).PosFunctionX   = [1 dps];
+                    Conditions(cond_num).PosFunctionX   = [1 0];
                     Conditions(cond_num).FuncFreqX 		= default_frequency;
-                    Conditions(cond_num).PosFuncNameX   = position_functions{dps};
+                    Conditions(cond_num).PosFuncNameX   = 'null';%position_functions{dps};
 
                     Conditions(cond_num).PosFunctionY 	= [2 0];
                     Conditions(cond_num).FuncFreqY 		= default_frequency;
                     Conditions(cond_num).PosFuncNameY   = 'null';
 
-                    Conditions(cond_num).Duration       = duration;
+                    Conditions(cond_num).Duration       = 100/dps;
                     Conditions(cond_num).note           = '';
 
                     total_ol_dur = total_ol_dur + Conditions(cond_num).Duration + .02;
@@ -114,14 +115,16 @@ for dps = 4:5 % 78 and 234 dps of the bar
     end
 end
 
-% Telethon ON OFF stimuli
+end
+
+% Telethon ON-OFF stimuli
 Conditions(cond_num).PatternID = 14;
 Conditions(cond_num).Duration = 3;
 Conditions(cond_num).InitialPosition = [1 1];
-Conditions(cond_num).Gains = [1 0 1 0];
+Conditions(cond_num).Gains = [3 0 3 0];
 Conditions(cond_num).Mode = [0 0];
 Conditions(cond_num).PosFunctionX = [1 0];
-Conditions(cond_num).PatternName = patterns{pattern};%'Pattern_22_expansion_on_foeleft_48_RC_telethon.mat';
+Conditions(cond_num).PatternName = patterns{Conditions(cond_num).PatternID};%'Pattern_22_expansion_on_foeleft_48_RC_telethon.mat';
 Conditions(cond_num).SpatialFreq = 30;
 Conditions(cond_num).PosFuncNameX = 'none';
 Conditions(cond_num).PosFuncLoc = 'R:\Telethon_Database\functions\telethon_pos_funcs_09_14';
@@ -135,10 +138,10 @@ cond_num = cond_num+ 1;
 Conditions(cond_num).PatternID = 15;
 Conditions(cond_num).Duration = 3;
 Conditions(cond_num).InitialPosition = [1 1];
-Conditions(cond_num).Gains = [1 0 1 0];
+Conditions(cond_num).Gains = [3 0 3 0];
 Conditions(cond_num).Mode = [0 0];
 Conditions(cond_num).PosFunctionX = [1 0];
-Conditions(cond_num).PatternName = patterns{pattern};%'Pattern_23_expansion_on_foeright_48_RC_telethon.mat';
+Conditions(cond_num).PatternName = patterns{Conditions(cond_num).PatternID};%'Pattern_23_expansion_on_foeright_48_RC_telethon.mat';
 Conditions(cond_num).SpatialFreq = 30;
 Conditions(cond_num).PosFuncNameX = 'none';
 Conditions(cond_num).PosFuncLoc = 'R:\Telethon_Database\functions\telethon_pos_funcs_09_14'; 
@@ -152,10 +155,10 @@ cond_num = cond_num+ 1;
 Conditions(cond_num).PatternID = 16;
 Conditions(cond_num).Duration = 3;
 Conditions(cond_num).InitialPosition = [1 1];
-Conditions(cond_num).Gains = [1 0 1 0];
+Conditions(cond_num).Gains = [3 0 3 0];
 Conditions(cond_num).Mode = [0 0];
 Conditions(cond_num).PosFunctionX = [1 0];
-Conditions(cond_num).PatternName = patterns{pattern};%'Pattern_24_expansion_off_foeleft_48_RC_telethon.mat';
+Conditions(cond_num).PatternName = patterns{Conditions(cond_num).PatternID};%'Pattern_24_expansion_off_foeleft_48_RC_telethon.mat';
 Conditions(cond_num).SpatialFreq = 30;
 Conditions(cond_num).PosFuncNameX = 'none';
 Conditions(cond_num).PosFuncLoc = 'R:\Telethon_Database\functions\telethon_pos_funcs_09_14'; 
@@ -169,10 +172,10 @@ cond_num = cond_num+ 1;
 Conditions(cond_num).PatternID = 17;
 Conditions(cond_num).Duration = 3;
 Conditions(cond_num).InitialPosition = [1 1];
-Conditions(cond_num).Gains = [1 0 1 0];
+Conditions(cond_num).Gains = [3 0 3 0];
 Conditions(cond_num).Mode = [0 0];
 Conditions(cond_num).PosFunctionX = [1 0];
-Conditions(cond_num).PatternName = patterns{pattern};%'Pattern_25_expansion_off_foeright_48_RC_telethon.mat';
+Conditions(cond_num).PatternName = patterns{Conditions(cond_num).PatternID};%'Pattern_25_expansion_off_foeright_48_RC_telethon.mat';
 Conditions(cond_num).SpatialFreq = 30;
 Conditions(cond_num).PosFuncNameX = 'none';
 Conditions(cond_num).PosFuncLoc = 'R:\Telethon_Database\functions\telethon_pos_funcs_09_14'; 
@@ -189,7 +192,7 @@ Conditions(cond_num).InitialPosition = [1 1];
 Conditions(cond_num).Gains = [32 0 0 0];
 Conditions(cond_num).Mode = [0 0];
 Conditions(cond_num).PosFunctionX = [1 0];
-Conditions(cond_num).PatternName = patterns{pattern};%'Pattern_21_on_off_motion_telethon_patterntelethon.mat';
+Conditions(cond_num).PatternName = patterns{Conditions(cond_num).PatternID};%'Pattern_21_on_off_motion_telethon_patterntelethon.mat';
 Conditions(cond_num).SpatialFreq = 30;
 Conditions(cond_num).PosFuncNameX = 'none';
 Conditions(cond_num).PosFuncLoc = 'R:\Telethon_Database\functions\telethon_pos_funcs_09_14'; 
@@ -206,7 +209,7 @@ Conditions(cond_num).InitialPosition = [1 2];
 Conditions(cond_num).Gains = [-32 0 0 0];
 Conditions(cond_num).Mode = [0 0];
 Conditions(cond_num).PosFunctionX = [1 0];
-Conditions(cond_num).PatternName = patterns{pattern};%'Pattern_21_on_off_motion_telethon_patterntelethon.mat';
+Conditions(cond_num).PatternName = patterns{Conditions(cond_num).PatternID};%'Pattern_21_on_off_motion_telethon_patterntelethon.mat';
 Conditions(cond_num).SpatialFreq = 30;
 Conditions(cond_num).PosFuncNameX = 'none';
 Conditions(cond_num).PosFuncLoc = 'R:\Telethon_Database\functions\telethon_pos_funcs_09_14'; 
@@ -223,7 +226,7 @@ Conditions(cond_num).InitialPosition = [1 3];
 Conditions(cond_num).Gains = [32 0 0 0];
 Conditions(cond_num).Mode = [0 0];
 Conditions(cond_num).PosFunctionX = [1 0];
-Conditions(cond_num).PatternName = patterns{pattern};%'Pattern_21_on_off_motion_telethon_patterntelethon.mat';
+Conditions(cond_num).PatternName = patterns{Conditions(cond_num).PatternID};%'Pattern_21_on_off_motion_telethon_patterntelethon.mat';
 Conditions(cond_num).SpatialFreq = 30;
 Conditions(cond_num).PosFuncNameX = 'none';
 Conditions(cond_num).PosFuncLoc = 'R:\Telethon_Database\functions\telethon_pos_funcs_09_14'; 
@@ -240,7 +243,7 @@ Conditions(cond_num).InitialPosition = [1 4];
 Conditions(cond_num).Gains = [-32 0 0 0];
 Conditions(cond_num).Mode = [0 0];
 Conditions(cond_num).PosFunctionX = [1 0];
-Conditions(cond_num).PatternName = patterns{pattern};%'Pattern_21_on_off_motion_telethon_pattern_8wide.mat';
+Conditions(cond_num).PatternName = patterns{Conditions(cond_num).PatternID};%'Pattern_21_on_off_motion_telethon_pattern_8wide.mat';
 Conditions(cond_num).PatternLoc = '/Users/stephenholtz/tethered_flight_arena_code/patterns/telethon_experiment_2012_patterns';
 Conditions(cond_num).PosFuncNameX = 'none';
 Conditions(cond_num).PosFuncNameY 	= 'null';

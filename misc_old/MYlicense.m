7582047e7b8d123cf83eed3217a50fdb5be423f9
Stephen Holtz <stephenlholtz@gmail.com>
2013-02-18 10:29:03 -0500

    Preparing for a large number of unilateral stimulus behavioral experiments, first round.

--- misc_old/MYlicense.m
+++
@@ -1,8 +0,0 @@
-function [varargout] = MYlicense(varargin)
-% overloaded license function for my own needs
-
-if strcmp(varargin{1},'test')
-    varargout{1} = 1;
-else
-    varargout = license(varargin);
-end
